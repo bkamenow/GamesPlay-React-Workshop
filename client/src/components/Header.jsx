19663f30f8e0c6fa8bf90e650e07ff5f7427dd9c
Borislav Kamenov <kamenowink@gmail.com>
2023-11-15 21:33:11 +0200

    fix navigation for user althentication

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <header>
             <h1>
@@ -10,14 +13,19 @@ export default function Header() {
             </h1>
             <nav>
                 <Link to='/catalogue'>All Games</Link>
-                <div id='user'>
-                    <Link to='/create-game'>Create Game</Link>
-                    <Link to='/logout'>Logout</Link>
-                </div>
-                <div id='guest'>
-                    <Link to='/login'>Login</Link>
-                    <Link to='/register'>Register</Link>
-                </div>
+                {isLoggedIn ? (
+                    // User is logged in
+                    <div id='user'>
+                        <Link to='/create-game'>Create Game</Link>
+                        <Link to='/logout'>Logout</Link>
+                    </div>
+                ) : (
+                    // User is not logged in
+                    <div id='guest'>
+                        <Link to='/login'>Login</Link>
+                        <Link to='/register'>Register</Link>
+                    </div>
+                )}
             </nav>
         </header>
     );
