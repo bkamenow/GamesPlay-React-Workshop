e7671816a0a238dc8ce5336d612d53d448e6859e
Borislav Kamenov <kamenowink@gmail.com>
2023-11-16 16:53:02 +0200

    add in home page to see 3 most recently added games if there are any

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { fetchRecentGames } from "../services/gamesService";
+
 export default function Home() {
+    const [recentGames, setRecentGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchRecentGamesData = async () => {
+            try {
+                const recentGamesData = await fetchRecentGames();
+                setRecentGames(recentGamesData);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching recent games:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchRecentGamesData();
+    }, []);
+
     return (
         <section id='welcome-world'>
             <div className='welcome-message'>
@@ -10,62 +32,35 @@ export default function Home() {
             <div id='home-page'>
                 <h1>Latest Games</h1>
 
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/CoverFire.png' />
-                    </div>
-                    <h3>Cover Fire</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/ZombieLang.png' />
-                    </div>
-                    <h3>Zombie Lang</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-                <div className='game'>
-                    <div className='image-wrap'>
-                        <img src='./images/MineCraft.png' />
-                    </div>
-                    <h3>MineCraft</h3>
-                    <div className='rating'>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                        <span>☆</span>
-                    </div>
-                    <div className='data-buttons'>
-                        <a href='#' className='btn details-btn'>
-                            Details
-                        </a>
-                    </div>
-                </div>
-
-                <p className='no-articles'>No games yet</p>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : recentGames.length > 0 ? (
+                    recentGames.map((game) => (
+                        <div key={game._id} className='game'>
+                            <div className='image-wrap'>
+                                <img src={game.imageUrl} alt={game.title} />
+                            </div>
+                            <h3>{game.title}</h3>
+                            <div className='rating'>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div className='data-buttons'>
+                                <Link
+                                    to={`/game-details/${game._id}`}
+                                    className='btn details-btn'
+                                >
+                                    Details
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className='no-articles'>No games yet</p>
+                )}
             </div>
         </section>
     );
--- client/src/services/gamesService.js
+++ client/src/services/gamesService.js
@@ -13,3 +13,20 @@ export const fetchGames = async () => {
         throw error;
     }
 };
+
+export const fetchRecentGames = async () => {
+    try {
+        const response = await fetch(`${baseURL}/jsonstore/games`);
+        if (!response.ok) {
+            throw new Error("Failed to fetch recent games");
+        }
+        const data = await response.json();
+        // Assuming data is an object with game IDs as keys
+        const gamesArray = Object.values(data);
+        const recentGames = gamesArray.slice(0, 3);
+        return recentGames;
+    } catch (error) {
+        console.error("Error fetching recent games:", error);
+        throw error;
+    }
+};
